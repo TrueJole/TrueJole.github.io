5782403304f4025013783eb73c058358e3953790
TrueJole <jole.koop@gmail.com>
2026-02-06 21:16:50 +0100

    tabea-test v2.0.11

--- tabea-test/index.service.worker.js
+++ tabea-test/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1770403340|376624860';
+const CACHE_VERSION = '1770408944|5980413348';
 /** @type {string} */
 const CACHE_PREFIX = 'Табеа-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
