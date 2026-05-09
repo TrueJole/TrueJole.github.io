0554975958a174e7bf1b7a4e844216289aa09bfc
TrueJole <jole.koop@gmail.com>
2026-05-09 19:54:02 +0200

    bug test

--- sandbags-helper-test/index.service.worker.js
+++ sandbags-helper-test/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1778349108|382543101';
+const CACHE_VERSION = '1778349230|504927297';
 /** @type {string} */
 const CACHE_PREFIX = 'ProjectSettingsT-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
