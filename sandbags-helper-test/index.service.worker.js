e3c5e3a7d5bbc91fc6dbb00b030e4b4fb712e6d1
TrueJole <jole.koop@gmail.com>
2026-05-14 11:09:31 +0200

    v0.10 test

--- sandbags-helper-test/index.service.worker.js
+++ sandbags-helper-test/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1778504006|289997740';
+const CACHE_VERSION = '1778749757|344066870';
 /** @type {string} */
 const CACHE_PREFIX = 'Sandbags Helper-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
